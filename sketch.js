6fc3766c14386f202f35d0b21b684659a7e5acee
ArhanShaikh770 <72856558+ArhanShaikh770@users.noreply.github.com>
2021-01-01 19:03:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var thief,police,obstacle;
+var score = 0;
+var gameState = 0;
+var cityBg;
+var policeI,thiefI;
+
+
+function preload(){
+cityBg = loadImage("Images/bg.jpg")
+policeI = loadAnimation("Images/Police/P1.png","Images/Police/P2.png","Images/Police/P3.png","Images/Police/P4.png","Images/Police/P5.png","Images/Police/P6.png","Images/Police/P7.png","Images/Police/P8.png");
+thiefI = loadAnimation("Images/Thief/T1.png","Images/Thief/T2.png","Images/Thief/T3.png","Images/Thief/T4.png","Images/Thief/T5.png","Images/Thief/T6.png");
+
+}
+
+
+function setup() {
+  createCanvas(1530,750);
+
+  city = createSprite(850,350,1700,700);
+  city.addImage(cityBg);
+  city.scale=1.2;
+  city.x = city.width/2;
+  //city.velocityX = 3
+
+  thief = createSprite(500, 650, 50, 50);
+  thief.addAnimation("thief_running",thiefI);
+  thief.scale = 0.8;
+
+  police = createSprite(100, 630, 50, 50);
+  police.addAnimation("police_running",policeI);
+  police.scale = 0.75;
+ 
+  
+}
+
+function draw() {
+ 
+  /*if(city.x > 700){
+    city.x = 400;
+  }*/
+
+
+
+  drawSprites();
+}
